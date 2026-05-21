1c549555db6577a28bd6c3b75153aee27980dfa4
luiz miguel lallemand valadares <luizmiguelvaladares454@gmail.com>
2026-05-20 22:30:56 -0300

    Add files via upload

---
+++ aula 15/script.js
@@ -0,0 +1,10 @@
+document.writeln("Olá,Mundo!");
+documente.writeln(<h2>Bem-vindo aojavaScript </h2>)
+window.alert("Esta é uma mensagem de alerta!");
+document.getElementById("title").
+innerhtml="Aprendendo
+JavaScrip com o melhor professor da UVV!!-JavaScrip
+
+
+
+
